47ec535ae016cf4b681ebad122327e4bb5a9be28
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-10-23 20:42:25 -0400

    Created a functioning form in ContractForm

--- src/components/ContactFrom.js
+++ src/components/ContactFrom.js
@@ -1,5 +1,45 @@
 import React from "react";
 
-export default function ContactForm() {
-  return <div>Contact Form</div>;
+export default function ContactForm({
+  name,
+  setName,
+  phone,
+  setPhone,
+  email,
+  setEmail,
+  handleSubmit,
+}) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        label="name"
+        value={name}
+        onChange={({ target }) => setName(target.value)}
+        required
+      />
+      <br />
+      <label htmlFor="phone">Phone: </label>
+      <input
+        type="tel"
+        aria-label="phone"
+        value={phone}
+        onChange={({ target }) => setPhone(target.value)}
+        patten="(\d{3})-(\d{3})-(\d{4})"
+        required
+      />
+      <br />
+      <label htmlFor="email">Email: </label>
+      <input
+        type="email"
+        aria-label="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+        required
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
--- src/containers/ContactsPage.js
+++ src/containers/ContactsPage.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ContactForm from "../components/ContactFrom";
 import TileList from "../components/TileList";
 
-export default function ContactsPage(props) {
+export default function ContactsPage({ contacts, addContact }) {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
@@ -11,25 +11,44 @@ export default function ContactsPage(props) {
 
   // Checking for name duplicates
   useEffect(() => {
-    for (contact of props.contacts) {
+    for (const contact of contacts) {
       if (name === contact.name) {
         setIsCopy(true);
       } else {
-        ssetIsCopy(false);
+        setIsCopy(false);
       }
     }
   }, [name]);
+
+  // Adds a new contact given the name is not a duplicate
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isCopy) {
+      addContact(name, phone, email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
+  };
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2>
-        <ContactForm />
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
-        <TileList />
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
